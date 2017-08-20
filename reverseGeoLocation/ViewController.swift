d07264b088ccd953aec99af62108e9a34ff3b14e
Amr Omran <amr.omran@econsor.de>
2017-08-20 23:23:35 +0200

    swift 3.0

--- reverseGeoLocation/ViewController.swift
+++ reverseGeoLocation/ViewController.swift
@@ -7,19 +7,67 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var physicalAddressLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+
+
+    var manager = CLLocationManager()
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.startUpdatingLocation()
+        manager.requestWhenInUseAuthorization()
+
+        mapView.delegate = self as? MKMapViewDelegate
+        mapView.showsUserLocation = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        // to get the location on the mapView
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let locationCoordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locationCoordinate, span: span)
+        mapView.setRegion(region, animated: true)
 
+        // to get the geoLocation
+        let lastLocation: CLLocation = locations[locations.count-1]
+        latitudeLabel.text = String(format: "%.6f", lastLocation.coordinate.latitude)
+        longitudeLabel.text = String(format: "%.6f", lastLocation.coordinate.longitude)
+
+        // to get the physical address
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemark, error) in
+            if (error != nil){
+                print("ERROR")
+            }
+            else{
+                if let place = placemark?[0]{
+                    if place.subThoroughfare != nil {
+                        self.physicalAddressLabel.text = "\(place.thoroughfare!) \(place.subThoroughfare!)\n\(place.locality!) \(place.postalCode!)\n\(place.country!)"
+                    }
+                    else {
+                        print("address error")
+                    }
+                }
+                else{
+                    print("place error")
+                }
+            }
+        }
+    }
 
 }
-
